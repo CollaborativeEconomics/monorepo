3277eee047cf84ffafb83df2a29011c758939bc7
Julian Kingman <julian.kingman@gmail.com>
2024-11-27 16:58:12 -0600

    add as files

---
+++ apps/give-cast/app/addnft/page.tsx
@@ -0,0 +1,64 @@
+'use client';
+import { useSearchParams } from 'next/navigation';
+import React, { Suspense } from 'react';
+
+const AddNFTContent: React.FC = () => {
+  const searchParams = useSearchParams();
+  const tokenId = searchParams.get('tokenId');
+
+  return (
+    <div
+      style={{
+        alignItems: 'center',
+        color: 'white',
+        background: '#334155',
+        display: 'flex',
+        flexDirection: 'column',
+        flexWrap: 'nowrap',
+        fontSize: 30,
+        fontStyle: 'normal',
+        letterSpacing: '-0.025em',
+        lineHeight: 1.4,
+        whiteSpace: 'pre-wrap',
+      }}
+    >
+      <h1 style={{ fontSize: 60 }}>Add NFT to Your MetaMask</h1>
+      <p style={{ margin: 0, padding: 0 }}>
+        Follow these steps to add your NFT to MetaMask:
+      </p>
+      <ol style={{ margin: 0, padding: 0 }}>
+        <li style={{ margin: 0, padding: 0 }}>
+          Open MetaMask and ensure you are connected to the correct network.
+        </li>
+        <li style={{ margin: 0, padding: 0 }}>Click on the "NFTs" tab.</li>
+        <li style={{ margin: 0, padding: 0 }}>
+          Click on "Import NFT" at the bottom of the page.
+        </li>
+        <li style={{ margin: 0, padding: 0 }}>
+          Enter the contract address:{' '}
+          <code style={{ fontSize: 20 }}>{process.env.MINTER_CONTRACT}</code>
+        </li>
+        <li style={{ margin: 0, padding: 0 }}>
+          Enter the token ID: <code style={{ fontSize: 20 }}>{tokenId}</code>
+        </li>
+        <li style={{ margin: 0, padding: 0 }}>
+          Click "Import" to add the NFT to your MetaMask.
+        </li>
+      </ol>
+      <p style={{ margin: 0, padding: 0, marginTop: 40, fontSize: 40 }}>
+        Once added, you should be able to view your NFT in the MetaMask
+        interface.
+      </p>
+    </div>
+  );
+};
+
+const AddNFTPage: React.FC = () => {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <AddNFTContent />
+    </Suspense>
+  );
+};
+
+export default AddNFTPage;
---
+++ apps/give-cast/utils/mailgun.ts
@@ -0,0 +1,25 @@
+import type { EmailBody } from "@/types"
+import formData from "form-data"
+import Mailgun from "mailgun.js"
+
+const mailgun = new Mailgun(formData)
+const mailgunClient = mailgun.client({
+  username: "api",
+  key: process.env.MAILGUN_API_KEY || "",
+})
+
+export const emailReceipt = async (email: string, body: EmailBody) => {
+  console.log("Emailing", email)
+  const emailResponse = await mailgunClient.messages
+    .create("givereceipts.cfce.io", {
+      from: "receipts@givecast.cfce.io",
+      to: email,
+      subject: `Donation Receipt - $${body.usdValue} ${body.organizationName}`,
+      template: "donation_receipt_universal",
+      "h:X-Mailgun-Variables": JSON.stringify(body),
+      "h:Reply-To": "evan@cfce.io",
+    })
+    .catch(console.error)
+  console.log("Mailing response", emailResponse)
+  return emailResponse
+}
