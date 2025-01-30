98ca3687433a2564bf57e8278cec45da1c06e0ed
Julian Kingman <julian.kingman@gmail.com>
2025-01-30 12:29:50 -0600

    fix story UI and links

--- packages/components/src/story/StoryCard.tsx
+++ packages/components/src/story/StoryCard.tsx
@@ -1,52 +1,57 @@
-import type { StoryWithRelations } from '@cfce/database';
-import Link from 'next/link';
-import React from 'react';
-import OrganizationAvatar from '~/organization/OrganizationAvatar';
-import { Card, CardContent, CardHeader } from '~/ui/card';
-import DateDisplay from '~/ui/date-posted';
-import Gallery from '~/ui/gallery';
+import type { StoryWithRelations } from "@cfce/database"
+import Link from "next/link"
+import React from "react"
+import OrganizationAvatar from "~/organization/OrganizationAvatar"
+import { Card, CardContent, CardFooter, CardHeader } from "~/ui/card"
+import DateDisplay from "~/ui/date-posted"
+import Gallery from "~/ui/gallery"
+import { Separator } from "../ui"
 
 interface StoryCardProps {
-  story: StoryWithRelations;
+  story: StoryWithRelations
 }
 
 export default function StoryCard(props: StoryCardProps) {
-  const story = props?.story;
+  const story = props?.story
   if (!story) {
-    return;
+    return
   }
-  const organization = story.organization;
-  const initiative = story.initiative;
-  const media = story.media.map(it => it.media); // flatten list
+  const organization = story.organization
+  const initiative = story.initiative
+  const media = story.media.map((it) => it.media) // flatten list
   if (story.image) {
-    media.unshift(story.image); // main image to the top
+    media.unshift(story.image) // main image to the top
   }
 
   return (
-    <Link href={`/stories/${story.id}`}>
-      <Card className="flex flex-col overflow-hidden">
-        <CardHeader>
+    <Card className="flex flex-col justify-between">
+      <div>
+        <CardHeader className="p-4 pt-6">
+          <DateDisplay timestamp={new Date(story.created).getTime()} />
+        </CardHeader>
+        <Link href={`/stories/${story.id}`}>
+          <div className="px-4">
+            <Gallery images={media} />
+          </div>
+          <CardContent className="flex flex-col p-4 gap-3">
+            <p>{story.description}</p>
+          </CardContent>
+        </Link>
+      </div>
+      <div>
+        <Separator />
+        <CardFooter className="flex-col gap-2 p-4 pb-6 self-start items-start">
           <OrganizationAvatar organization={organization} />
           <p className="text-sm font-semibold">
-            in{' '}
+            in{" "}
             <span className="underline">
               <Link href={`/initiatives/${initiative?.id}`}>
                 {initiative?.title}
               </Link>
             </span>
           </p>
-          <DateDisplay
-            timestamp={new Date(story.created).getTime()}
-            className="py-4"
-          />
-        </CardHeader>
-        <div className="px-2 -mt-2">
-          <Gallery images={media} />
-        </div>
-        <CardContent className="flex flex-col pb-8 pt-3 gap-3 px-0">
-          <p className="px-6">{story.description}</p>
-        </CardContent>
-      </Card>
-    </Link>
-  );
+        </CardFooter>
+      </div>
+    </Card>
+  )
 }
