575a6dfad305ca05f7220c956ca2a5db52ee169e
Julian Kingman <julian.kingman@gmail.com>
2025-02-12 20:59:03 -0600

    dynamic network for v2e factory

--- apps/partners/src/app/dashboard/events/reward/[id]/page.tsx
+++ apps/partners/src/app/dashboard/events/reward/[id]/page.tsx
@@ -1,33 +1,35 @@
-import { getContract, getEventById } from '@cfce/database';
-import { Suspense } from 'react';
-import { getReportedAddresses } from '~/utils/chainLogs';
-import RewardClient from './reward-client';
+import appConfig from "@cfce/app-config"
+import { getContract, getEventById } from "@cfce/database"
+import { Suspense } from "react"
+import { getReportedAddresses } from "~/utils/chainLogs"
+import RewardClient from "./reward-client"
 
 interface PageProps {
-  params: Promise<{ id: string }>;
+  params: Promise<{ id: string }>
   //searchParams: Promise<{ [key: string]: string | string[] | undefined }>
 }
 
 export default async function RewardPage({ params }: PageProps) {
-  const { id } = await params;
-  const eventData = await getEventById(id);
-  const event = JSON.parse(JSON.stringify(eventData));
+  const { id } = await params
+  const eventData = await getEventById(id)
+  const event = JSON.parse(JSON.stringify(eventData))
   //console.log('EVENT', event)
   if (!event) {
     // TODO: Handle redirect in middleware or return not found
-    return null;
+    return null
   }
 
-  const resNFT = await getContract(id, 'arbitrum', 'testnet', '1155');
-  const contractNFT = resNFT.length > 0 ? resNFT[0] : null;
+  const network = appConfig.chainDefaults.network
+  const resNFT = await getContract(id, "arbitrum", network, "1155")
+  const contractNFT = resNFT.length > 0 ? resNFT[0] : null
   //console.log('NFT-CTR', contractNFT)
 
   if (!contractNFT) {
-    return null;
+    return null
   }
 
-  const resV2E = await getContract(id, 'arbitrum', 'testnet', 'V2E');
-  const contractV2E = resV2E.length > 0 ? resV2E[0] : null;
+  const resV2E = await getContract(id, "arbitrum", network, "V2E")
+  const contractV2E = resV2E.length > 0 ? resV2E[0] : null
   //console.log('V2E-CTR', contractV2E)
 
   if (
@@ -35,14 +37,14 @@ export default async function RewardPage({ params }: PageProps) {
     !contractNFT.contract_address ||
     !contractNFT.start_block
   ) {
-    return null;
+    return null
   }
 
   const { data: volunteers } = await getReportedAddresses(
     contractNFT.contract_address,
     contractNFT.start_block,
-  );
-  console.log('VOLUNTEERS', volunteers, contractNFT, contractV2E);
+  )
+  console.log("VOLUNTEERS", volunteers, contractNFT, contractV2E)
 
   return (
     <Suspense fallback={<div>Loading...</div>}>
@@ -54,5 +56,5 @@ export default async function RewardPage({ params }: PageProps) {
         contractV2E={contractV2E}
       />
     </Suspense>
-  );
+  )
 }
