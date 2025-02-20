e804486c551d56206172e487bcab03624780b48b
Julian Kingman <julian.kingman@gmail.com>
2025-02-20 11:06:18 -0600

    update volunteer contract code

--- packages/blockchain-tools/src/contracts/solidity/volunteers/DistributorAbi.ts
+++ packages/blockchain-tools/src/contracts/solidity/volunteers/DistributorAbi.ts
@@ -1,309 +1,226 @@
 export const VolunteersDistributorAbi = [
   {
-    "type": "constructor",
-    "inputs": [
+    type: "constructor",
+    inputs: [
       {
-        "name": "_token",
-        "type": "address",
-        "internalType": "address"
+        name: "_token",
+        type: "address",
+        internalType: "address",
       },
       {
-        "name": "_owner",
-        "type": "address",
-        "internalType": "address"
+        name: "_owner",
+        type: "address",
+        internalType: "address",
       },
       {
-        "name": "_nftContract",
-        "type": "address",
-        "internalType": "contract ERC1155"
+        name: "_nftContract",
+        type: "address",
+        internalType: "contract ERC1155",
       },
       {
-        "name": "_baseFee",
-        "type": "uint256",
-        "internalType": "uint256"
-      }
+        name: "_baseFee",
+        type: "uint256",
+        internalType: "uint256",
+      },
     ],
-    "stateMutability": "nonpayable"
+    stateMutability: "nonpayable",
   },
   {
-    "type": "receive",
-    "stateMutability": "payable"
+    type: "receive",
+    stateMutability: "payable",
   },
   {
-    "type": "function",
-    "name": "baseFee",
-    "inputs": [],
-    "outputs": [
+    type: "function",
+    name: "changeTokenAddress",
+    inputs: [
       {
-        "name": "",
-        "type": "uint256",
-        "internalType": "uint256"
-      }
+        name: "tokenAddress",
+        type: "address",
+        internalType: "address",
+      },
     ],
-    "stateMutability": "view"
+    outputs: [],
+    stateMutability: "nonpayable",
   },
   {
-    "type": "function",
-    "name": "changeTokenAddress",
-    "inputs": [
+    type: "function",
+    name: "distributeTokensByUnit",
+    inputs: [
       {
-        "name": "tokenAddress",
-        "type": "address",
-        "internalType": "address"
-      }
+        name: "recipients",
+        type: "address[]",
+        internalType: "address[]",
+      },
     ],
-    "outputs": [],
-    "stateMutability": "nonpayable"
+    outputs: [],
+    stateMutability: "nonpayable",
   },
   {
-    "type": "function",
-    "name": "distributeTokensByUnit",
-    "inputs": [
+    type: "function",
+    name: "getBaseFee",
+    inputs: [],
+    outputs: [
       {
-        "name": "recipients",
-        "type": "address[]",
-        "internalType": "address[]"
-      }
+        name: "_baseFee",
+        type: "uint256",
+        internalType: "uint256",
+      },
     ],
-    "outputs": [],
-    "stateMutability": "nonpayable"
+    stateMutability: "view",
   },
   {
-    "type": "function",
-    "name": "getBaseFee",
-    "inputs": [],
-    "outputs": [
+    type: "function",
+    name: "getNFTAddress",
+    inputs: [],
+    outputs: [
       {
-        "name": "_baseFee",
-        "type": "uint256",
-        "internalType": "uint256"
-      }
+        name: "",
+        type: "address",
+        internalType: "address",
+      },
     ],
-    "stateMutability": "view"
+    stateMutability: "view",
   },
   {
-    "type": "function",
-    "name": "getToken",
-    "inputs": [],
-    "outputs": [
+    type: "function",
+    name: "getToken",
+    inputs: [],
+    outputs: [
       {
-        "name": "_token",
-        "type": "address",
-        "internalType": "address"
-      }
+        name: "_token",
+        type: "address",
+        internalType: "address",
+      },
     ],
-    "stateMutability": "view"
+    stateMutability: "view",
   },
   {
-    "type": "function",
-    "name": "getWhitelistedAddresses",
-    "inputs": [],
-    "outputs": [
+    type: "function",
+    name: "owner",
+    inputs: [],
+    outputs: [
       {
-        "name": "whitelist",
-        "type": "address[]",
-        "internalType": "address[]"
-      }
+        name: "",
+        type: "address",
+        internalType: "address",
+      },
     ],
-    "stateMutability": "view"
+    stateMutability: "view",
   },
   {
-    "type": "function",
-    "name": "isWhitelisted",
-    "inputs": [
+    type: "function",
+    name: "s_baseFee",
+    inputs: [],
+    outputs: [
       {
-        "name": "user",
-        "type": "address",
-        "internalType": "address"
-      }
+        name: "",
+        type: "uint256",
+        internalType: "uint256",
+      },
     ],
-    "outputs": [
-      {
-        "name": "status",
-        "type": "bool",
-        "internalType": "bool"
-      }
-    ],
-    "stateMutability": "view"
+    stateMutability: "view",
   },
   {
-    "type": "function",
-    "name": "nftContract",
-    "inputs": [],
-    "outputs": [
+    type: "function",
+    name: "s_registeredAddresses",
+    inputs: [
       {
-        "name": "",
-        "type": "address",
-        "internalType": "contract ERC1155"
-      }
+        name: "",
+        type: "uint256",
+        internalType: "uint256",
+      },
     ],
-    "stateMutability": "view"
+    outputs: [
+      {
+        name: "",
+        type: "address",
+        internalType: "address",
+      },
+    ],
+    stateMutability: "view",
   },
   {
-    "type": "function",
-    "name": "owner",
-    "inputs": [],
-    "outputs": [
+    type: "function",
+    name: "s_token",
+    inputs: [],
+    outputs: [
       {
-        "name": "",
-        "type": "address",
-        "internalType": "address"
-      }
+        name: "",
+        type: "address",
+        internalType: "address",
+      },
     ],
-    "stateMutability": "view"
+    stateMutability: "view",
   },
   {
-    "type": "function",
-    "name": "registeredAddresses",
-    "inputs": [
+    type: "function",
+    name: "transferOwnership",
+    inputs: [
       {
-        "name": "",
-        "type": "uint256",
-        "internalType": "uint256"
-      }
+        name: "newOwner",
+        type: "address",
+        internalType: "address",
+      },
     ],
-    "outputs": [
-      {
-        "name": "",
-        "type": "address",
-        "internalType": "address"
-      }
-    ],
-    "stateMutability": "view"
+    outputs: [],
+    stateMutability: "nonpayable",
   },
   {
-    "type": "function",
-    "name": "removeFromWhitelist",
-    "inputs": [
+    type: "function",
+    name: "updateBaseFee",
+    inputs: [
       {
-        "name": "user",
-        "type": "address",
-        "internalType": "address"
-      }
+        name: "_baseFee",
+        type: "uint256",
+        internalType: "uint256",
+      },
     ],
-    "outputs": [],
-    "stateMutability": "nonpayable"
+    outputs: [],
+    stateMutability: "nonpayable",
   },
   {
-    "type": "function",
-    "name": "token",
-    "inputs": [],
-    "outputs": [
-      {
-        "name": "",
-        "type": "address",
-        "internalType": "address"
-      }
-    ],
-    "stateMutability": "view"
+    type: "function",
+    name: "withdrawToken",
+    inputs: [],
+    outputs: [],
+    stateMutability: "nonpayable",
   },
   {
-    "type": "function",
-    "name": "transferOwnership",
-    "inputs": [
+    type: "event",
+    name: "OwnershipTransferred",
+    inputs: [
       {
-        "name": "newOwner",
-        "type": "address",
-        "internalType": "address"
-      }
-    ],
-    "outputs": [],
-    "stateMutability": "nonpayable"
-  },
-  {
-    "type": "function",
-    "name": "updateBaseFee",
-    "inputs": [
-      {
-        "name": "_baseFee",
-        "type": "uint256",
-        "internalType": "uint256"
-      }
-    ],
-    "outputs": [],
-    "stateMutability": "nonpayable"
-  },
-  {
-    "type": "function",
-    "name": "updateWhitelist",
-    "inputs": [
-      {
-        "name": "user",
-        "type": "address",
-        "internalType": "address"
-      }
-    ],
-    "outputs": [],
-    "stateMutability": "nonpayable"
-  },
-  {
-    "type": "function",
-    "name": "whitelistAddresses",
-    "inputs": [
-      {
-        "name": "_addresses",
-        "type": "address[]",
-        "internalType": "address[]"
-      }
-    ],
-    "outputs": [],
-    "stateMutability": "nonpayable"
-  },
-  {
-    "type": "function",
-    "name": "whitelisted",
-    "inputs": [
-      {
-        "name": "",
-        "type": "address",
-        "internalType": "address"
-      }
-    ],
-    "outputs": [
-      {
-        "name": "",
-        "type": "bool",
-        "internalType": "bool"
-      }
-    ],
-    "stateMutability": "view"
-  },
-  {
-    "type": "event",
-    "name": "OwnershipTransferred",
-    "inputs": [
-      {
-        "name": "user",
-        "type": "address",
-        "indexed": true,
-        "internalType": "address"
+        name: "user",
+        type: "address",
+        indexed: true,
+        internalType: "address",
       },
       {
-        "name": "newOwner",
-        "type": "address",
-        "indexed": true,
-        "internalType": "address"
-      }
+        name: "newOwner",
+        type: "address",
+        indexed: true,
+        internalType: "address",
+      },
     ],
-    "anonymous": false
+    anonymous: false,
   },
   {
-    "type": "event",
-    "name": "TokensDistributed",
-    "inputs": [
+    type: "event",
+    name: "TokensDistributed",
+    inputs: [
       {
-        "name": "recipient",
-        "type": "address",
-        "indexed": true,
-        "internalType": "address"
+        name: "recipient",
+        type: "address",
+        indexed: true,
+        internalType: "address",
       },
       {
-        "name": "amount",
-        "type": "uint256",
-        "indexed": false,
-        "internalType": "uint256"
-      }
+        name: "amount",
+        type: "uint256",
+        indexed: false,
+        internalType: "uint256",
+      },
     ],
-    "anonymous": false
-  }
-] as const;
+    anonymous: false,
+  },
+] as const
