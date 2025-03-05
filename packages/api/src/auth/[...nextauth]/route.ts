1beb5776cf0aba38eaf6eead68751ae82fda65a2
Lawal Abubakar Babatunde <utility4all@gmail.com>
2025-03-05 18:40:04 +0100

    Chore: testing

--- packages/api/src/auth/[...nextauth]/route.ts
+++ packages/api/src/auth/[...nextauth]/route.ts
@@ -1,5 +1,12 @@
 import { handlers } from "@cfce/auth"
+import type { NextRequest } from "next/server"
 
 export const dynamic = "force-dynamic"
 
-export const { GET, POST } = handlers
+export const { 
+    GET, 
+    POST 
+  }: {
+GET: (request: NextRequest) => Promise<Response>
+  POST: (request: NextRequest) => Promise<Response>
+} = handlers
