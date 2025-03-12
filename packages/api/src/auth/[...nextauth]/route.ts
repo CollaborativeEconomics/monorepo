a3d262d9ae095a2bf80e97645b8867368db01c54
Lawal Abubakar Babatunde <utility4all@gmail.com>
2025-03-12 15:20:51 +0100

    Testing against live website

--- packages/api/src/auth/[...nextauth]/route.ts
+++ packages/api/src/auth/[...nextauth]/route.ts
@@ -6,7 +6,4 @@ export const dynamic = "force-dynamic"
 export const { 
     GET, 
     POST 
-  }: {
-GET: (request: NextRequest) => Promise<Response>
-  POST: (request: NextRequest) => Promise<Response>
-} = handlers
+  } = handlers
