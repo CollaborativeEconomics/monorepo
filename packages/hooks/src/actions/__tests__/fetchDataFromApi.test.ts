d48a58c6795f76b9813ab7ca32a379fb3707f424
Julian Kingman <julian.kingman@gmail.com>
2025-03-13 14:50:13 -0500

    convert tests from bun to jest

--- packages/hooks/src/actions/__tests__/fetchDataFromApi.test.ts
+++ packages/hooks/src/actions/__tests__/fetchDataFromApi.test.ts
@@ -1,12 +1,17 @@
-import server from "../../../mocks/serverMock";
-import { expect, test, describe, beforeAll, afterEach, afterAll } from "bun:test";
-import { getRegistryBaseUrl } from "../../init";
-
+import {
+  afterAll,
+  afterEach,
+  beforeAll,
+  describe,
+  expect,
+  test,
+} from "@jest/globals"
+import server from "../../../mocks/serverMock"
 
 describe("fetchDataFromApi", async () => {
   test("fetches data from the API", async () => {
-    const response = await fetch(`${getRegistryBaseUrl()}/test`);
-    const data = await response.json();
-    expect(data).toEqual({ message: "Hello test!" });
+    const response = await fetch("https://registry.cfce.io/api/test")
+    const data = await response.json()
+    expect(data).toEqual({ message: "Hello test!" })
   })
 })
