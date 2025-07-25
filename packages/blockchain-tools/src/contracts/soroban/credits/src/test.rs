f86179a62bb0104325536db6931ee80b1509c89e
dchagast <stepcek80@gmail.com>
2025-07-25 11:13:25 -0700

    fix comments

--- packages/blockchain-tools/src/contracts/soroban/credits/src/test.rs
+++ packages/blockchain-tools/src/contracts/soroban/credits/src/test.rs
@@ -4,17 +4,10 @@ extern crate std;
 use std::{println as info, println as warn};
 use crate::{contract::Credits, CreditsClient};
 use soroban_sdk::{
-  testutils::Address as _,
-  Address,
-  Env,
-  String,
-  token,
+  testutils::Address as _, token, Address, Env, String
 };
 
-use crate::test_utils::{
-  deploy_native_sac,
-  create_account_entry
-};
+use crate::test_utils::{deploy_native_sac, create_account_entry};
 
 fn create_contract<'a>(
   e: &Env,
@@ -48,14 +41,14 @@ fn test_views() {
   let e = Env::default();
   e.mock_all_auths();
 
-  let admin   = Address::generate(&e);
-  let bucket     = 200000000i128;
-  let initiative = String::from_str(&e, "30c0636f-b0f1-40d5-bb9c-a531dc4d69e2");
-  let provider   = Address::generate(&e);
-  let vendor     = Address::generate(&e);
+  let admin = Address::generate(&e);
+  let bucket = 200_000_000_i128;
+  let initiative: String = String::from_str(&e, "30c0636f-b0f1-40d5-bb9c-a531dc4d69e2");
+  let provider = Address::generate(&e);
+  let vendor = Address::generate(&e);
   let xlm = deploy_native_sac(&e);
 
-  let credit     = create_contract(
+  let credit = create_contract(
     &e,
     &admin,
     &initiative,
@@ -85,19 +78,19 @@ fn test_donate() {
   let e = Env::default();
   e.mock_all_auths();
 
-  let admin      = Address::generate(&e);
-  let bucket        = 200000000i128;
+  let admin = Address::generate(&e);
+  let bucket = 200000000i128;
   let donor_pubkey = "GA2H3SJYGIUG2DXXUZ7IN3LNO2AIMVWCDCL25PKQHKMC76OWW3HYQHY4";
-  let donor = Address::from_str(&e, &donor_pubkey);
+  let donor = Address::from_str(&e, donor_pubkey);
 
   let initiative = String::from_str(&e, "30c0636f-b0f1-40d5-bb9c-a531dc4d69e2");
-  let provider   = Address::generate(&e);
-  let vendor     = Address::generate(&e);
+  let provider = Address::generate(&e);
+  let vendor = Address::generate(&e);
   
   let xlm = deploy_native_sac(&e);
   let xlm_client = token::Client::new(&e, &xlm);
 
-  let credit     = create_contract(
+  let credit = create_contract(
     &e,
     &admin,
     &initiative,
@@ -108,8 +101,11 @@ fn test_donate() {
   );
 
   create_account_entry(&e, &donor_pubkey);
+  assert_eq!(xlm_client.balance(&donor), 10_000_000_000);
 
   // Donate
-  credit.donate(&donor, &100000000);
-  assert_eq!(credit.getBalance(), 90000000); // amount - fees
+  credit.donate(&donor, &100_000_000);
+  assert_eq!(credit.getBalance(), 90_000_000); // amount - vendor fees
+  assert_eq!(credit.getContractBalance(), 90_000_000);
+  assert_eq!(xlm_client.balance(&donor), 9_900_000_000);
 }
